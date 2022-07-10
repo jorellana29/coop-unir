a6a79e42a92638194644ab034864bf7de3cba4b1
jorellana29 <jorellana29@gmail.com>
2022-07-10 14:34:56 -0500

    fork

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'coop-unir',
-      exec_mode: 'cluster',
+      exec_mode: 'fork',
       instances: '1',
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
